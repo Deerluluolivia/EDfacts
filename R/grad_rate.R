cb57be789e3cff84efe880aaeb7fdea5c61da3b1
Lu Han <lhan@nps.k12.nj.us>
2018-03-15 16:29:33 -0400

    replace supression rules and calculate the midpoint

--- R/grad_rate.R
+++ R/grad_rate.R
@@ -24,12 +24,57 @@ get_raw_grad_rate <- function(end_year){
   read_csv(tname) %>%
     janitor::clean_names() %>%
     mutate(end_year = end_year)
-
 }
 
 
 tidy_grad_rate <- function(df) {
-  gsub('_\\d\\d\\d\\d$', '', names(df))
+ df <- as.data.frame(df)
+ #get rid of the ending year in variable names, eg. _1011
+ names(df) <-  gsub('_\\d\\d\\d\\d$', '', names(df))
+
+ #get rid of "date_cur"
+ df$date_cur <- NULL
+
+ #deal with suppression rules. GE is >=, LE is <=, GT is >, LT is <, PS is suppressed
+ rate_cols <- grepl('_rate', names(df))
+ constant_df <- df[, !rate_cols]
+ rate_df <- df[, rate_cols]
+
+ rate_df <- rate_df %>%
+   mutate_all(
+     function(x) {
+       #for greater than/ less than, drop the string, leave the number
+       x <- gsub('GE|LE|GT|LT', '', x)
+       x <- gsub('PS', NA_character_, x)
+       x
+     }
+   )
+
+ df <- bind_cols(constant_df, rate_df)
+
+ extract_mean <- function(df, s) {
+   df <- df %>% separate(s, "-", into=c("first", "second"))
+
+   df$first <- as.numeric(df$first)
+   df$second <- as.numeric(df$second)
+
+   df$mean <- ifelse(!is.na(df$second),
+                     (df$first + df$second)/2,
+                     df$first)
+   df %>% select(-first, -second)
+ }
+ extract_mean(df, 'all_rate')
+
+  df %>%
+    mutate_at(
+      c('all_rate'),
+      extract_mean
+    )
+
+ #calculate the midpoint for suppressed data with a range, eg. 70-75
+ df[rate,] <- lapply(df[rate,], extract.mean)
+
+ return(df)
 }
 
 
@@ -37,3 +82,4 @@ fetch_grad_rate <- function(end_year) {
   get_raw_grad_rate(end_year) %>%
     tidy_grad_rate()
 }
+
